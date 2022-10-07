79856def1cf03c1a0ed0d2a36e57481911c814d3
Juan Araneta <juanaraneta10@gmail.com>
2022-10-07 08:17:51 -0300

    Remove Functionality

--- node/dist/services/launches.js
+++ node/dist/services/launches.js
@@ -12,29 +12,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.processLaunches = void 0;
 /* eslint-disable camelcase */
 const favorites_1 = require("./favorites");
-const findRocket = (rockets, rocketId) => rockets.find((rocket) => rocket.rocket_id === rocketId);
-const findFavorite = (favorites, flightNumber) => favorites.find((favorite) => favorite.flight_number === flightNumber);
 const processLaunches = (userId, launches, rockets) => __awaiter(void 0, void 0, void 0, function* () {
     const userFavorites = yield (0, favorites_1.getUserFavorites)(userId);
-    return launches.map((launch) => {
-        const { details, launch_date_unix, flight_number, mission_name, links: { mission_patch }, rocket: { rocket_name, rocket_id } } = launch;
-        const { cost_per_launch, company, active } = findRocket(rockets, rocket_id);
-        const foundFavorite = findFavorite(userFavorites, flight_number);
-        return {
-            flight_number,
-            mission_name,
-            mission_patch,
-            details,
-            launch_date_unix,
-            rocket: {
-                rocket_id,
-                rocket_name,
-                active,
-                cost_per_launch,
-                company
-            },
-            favorite: !!foundFavorite
-        };
-    });
+    return [];
 });
 exports.processLaunches = processLaunches;
