76c3ae1ac2b4aa51e0a6539407284edff27f25be
Juan Araneta <juanaraneta10@gmail.com>
2022-10-07 08:30:13 -0300

    Remove auth funct

--- node/dist/middlewares/auth.js
+++ node/dist/middlewares/auth.js
@@ -2,19 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.auth = void 0;
 const auth = (req, res, next) => {
-    // const jwtSecretKey = process.env.JWT_SECRET_KEY;
-    // try {
-    //   const token = req.header("Authorization");
-    //   if (!token) {
-    //     return res
-    //       .status(400)
-    //       .send({ meesage: "Authorization header is needed" });
-    //   }
-    //   const data: any = jwt.verify(token, jwtSecretKey);
-    //   if (!data) {
-    //     return res.status(401).send({ meesage: "Invalid Authorization token" });
-    //   }
-    //   req.currentUserId = data.userId;
     return next();
 };
 exports.auth = auth;
--- node/src/middlewares/auth.ts
+++ node/src/middlewares/auth.ts
@@ -1,21 +1,3 @@
 export const auth = (req, res, next) => {
-  // const jwtSecretKey = process.env.JWT_SECRET_KEY;
-  // try {
-  //   const token = req.header("Authorization");
-
-  //   if (!token) {
-  //     return res
-  //       .status(400)
-  //       .send({ meesage: "Authorization header is needed" });
-  //   }
-
-  //   const data: any = jwt.verify(token, jwtSecretKey);
-  //   if (!data) {
-  //     return res.status(401).send({ meesage: "Invalid Authorization token" });
-  //   }
-
-  //   req.currentUserId = data.userId;
-
   return next();
-  
 };
