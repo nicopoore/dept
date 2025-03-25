2d629372cda560af88ab81bef76dcece0212e737
Nicolás Poore <nicolaspoore@gmail.com>
2025-03-24 21:20:09 -0300

    chore: remove logs

--- node/src/controllers/launches.ts
+++ node/src/controllers/launches.ts
@@ -4,9 +4,7 @@ import { processLaunches } from '../services/launches'
 export const getLaunches = async (req, res) => {
   const userId = req.currentUserId
   const [launches, rockets] = await Promise.all([fetchLaunches(), fetchRockets()])
-  console.log({ launches, rockets })
   const outputLaunches = await processLaunches(userId, launches, rockets)
-  console.log({ outputLaunches })
 
   return res.status(200).json(outputLaunches)
 }
